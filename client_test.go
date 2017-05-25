0c7a10b14e908dc839a703c8675cda0a4dde8430
Michael Crosby <crosbymichael@gmail.com>
2017-05-25 13:52:42 -0700

    Add integration tests for running containers
    Add travis support for running integration tests with the client package
    and go test framework
    
    Signed-off-by: Michael Crosby <crosbymichael@gmail.com>

--- client_test.go
+++ client_test.go
@@ -3,12 +3,43 @@ package containerd
 import (
 	"context"
 	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
 	"testing"
 )
 
 func init() {
 	flag.StringVar(&address, "address", "/run/containerd/containerd.sock", "The address to the containerd socket for use in the tests")
 	flag.Parse()
+}
+
+const defaultRoot = "/var/lib/containerd-test"
+
+func TestMain(m *testing.M) {
+	// setup a new containerd daemon
+	cmd := exec.Command("containerd", "--root", defaultRoot)
+	// TODO: what todo with IO?
+	if err := cmd.Start(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	status := m.Run()
+
+	// tear down the daemon and resources created
+	if err := cmd.Process.Signal(syscall.SIGTERM); err != nil {
+		fmt.Println(err)
+	}
+	if _, err := cmd.Process.Wait(); err != nil {
+		fmt.Println(err)
+	}
+	if err := os.RemoveAll(defaultRoot); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	os.Exit(status)
 }
 
 var address string
